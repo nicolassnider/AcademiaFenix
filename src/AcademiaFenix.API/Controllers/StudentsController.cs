1a9915781927d585c6af281dcb87f6ebd40d9ac3
Nicolás Snider <nicolas.snider@outlook.com>
2024-05-19 16:50:16 -0300

    API. Add method controller to findbyId
    Application.
    Add Student getById query

--- src/AcademiaFenix.API/Controllers/StudentsController.cs
+++ src/AcademiaFenix.API/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using AcademiaFenix.Application.Students;
+﻿using AcademiaFenix.Application;
+using AcademiaFenix.Application.Students;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +7,8 @@ namespace AcademiaFenix.API.Controllers
 {
     public class StudentsController : BaseApiController
     {
-        public StudentsController(IMediator mediator) : base(mediator)
-        {
-
-        }
+        public StudentsController(IMediator mediator)
+            : base(mediator) { }
 
         [HttpGet]
         public async Task<IActionResult> GetStudents([FromQuery] StudentParams studentParams)
@@ -18,6 +17,16 @@ namespace AcademiaFenix.API.Controllers
 
             var result = await _mediator.Send(query);
             return HandlePagedResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudent(Guid id)
+        {
+            var query = new GetById.Query { Id = id };
+
+            var result = await _mediator.Send(query);
+
+            return HandleResult(result);
         }
     }
 }
